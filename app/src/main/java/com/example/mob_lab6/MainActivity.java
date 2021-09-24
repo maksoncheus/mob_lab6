8523a01e326c7520fd6ad6cb115a1898940963c2
maksoncheus <maksoncheus@gmail.com>
2021-09-25 01:03:48 +0700

    всё поломалось

--- app/src/main/java/com/example/mob_lab6/MainActivity.java
+++ app/src/main/java/com/example/mob_lab6/MainActivity.java
@@ -3,12 +3,46 @@ package com.example.mob_lab6;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    String name = "undefined";
+    final static String nameVariableKey = "NAME_VAR";
+    final static String textViewTexKey = "TEXT_VIEW";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString(nameVariableKey, name);
+        TextView nameView = (TextView) findViewById(R.id.saveTextView);
+        outState.putString(textViewTexKey, nameView.getText().toString());
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        name = savedInstanceState.getString(nameVariableKey);
+        String textViewText = savedInstanceState.getString(textViewTexKey);
+        TextView nameView = (TextView) findViewById(R.id.saveTextView);
+        nameView.setText(textViewText);
+    }
+
+    public void restoreField(View view) {
+        TextView nameView = (TextView) findViewById(R.id.saveTextView);
+        nameView.setText(name);
+    }
+
+    public void saveField(View view) {
+        TextView nameBox = (TextView) findViewById(R.id.editTextTextMultiLine);
+        name = nameBox.getText().toString();
+    }
 }
+
+
