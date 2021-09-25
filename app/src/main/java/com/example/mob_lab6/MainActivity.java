d59c052855db8685dc9ac8709ad00b629d46149f
maksoncheus <maksoncheus@gmail.com>
2021-09-25 16:27:16 +0700

    переделал под счетчик нажатий. onSaveInstanceState и onRestoreInstanceState вызываются при каждом повороте экрана, поэтому при обращении к элементов по факту другого активити, приложение просто закрывается.

--- app/src/main/java/com/example/mob_lab6/MainActivity.java
+++ app/src/main/java/com/example/mob_lab6/MainActivity.java
@@ -7,42 +7,30 @@ import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    String name = "undefined";
-    final static String nameVariableKey = "NAME_VAR";
-    final static String textViewTexKey = "TEXT_VIEW";
-
+    int cntr = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
     @Override
-    protected void onSaveInstanceState(Bundle outState) {
+    protected void onSaveInstanceState(Bundle outState)
+    {
+        outState.putInt("counter", cntr);
         super.onSaveInstanceState(outState);
-        outState.putString(nameVariableKey, name);
-        TextView nameView = (TextView) findViewById(R.id.saveTextView);
-        outState.putString(textViewTexKey, nameView.getText().toString());
     }
 
     @Override
-    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+    protected void onRestoreInstanceState(Bundle savedInstanceState)
+    {
         super.onRestoreInstanceState(savedInstanceState);
-        name = savedInstanceState.getString(nameVariableKey);
-        String textViewText = savedInstanceState.getString(textViewTexKey);
-        TextView nameView = (TextView) findViewById(R.id.saveTextView);
-        nameView.setText(textViewText);
+        cntr = savedInstanceState.getInt("counter");
     }
 
-    public void restoreField(View view) {
-        TextView nameView = (TextView) findViewById(R.id.saveTextView);
-        nameView.setText(name);
-    }
 
-    public void saveField(View view) {
-        TextView nameBox = (TextView) findViewById(R.id.editTextTextMultiLine);
-        name = nameBox.getText().toString();
+    public void btn_click(View view)
+    {
+        TextView txt = (TextView)findViewById(R.id.textView);
+        txt.setText("Вы нажали на кнопку " + ++cntr + " раз");
     }
 }
-
-
